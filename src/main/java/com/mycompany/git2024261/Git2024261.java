284e9c8c1eae998fbb0f5fc91451b6bd6e435e8f
yheyc <yheyc@Yheycko>
2024-12-31 15:17:46 +0000

    Project

---
+++ src/main/java/com/mycompany/git2024261/Git2024261.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git2024261;
+
+/**
+ *
+ * @author yheyc
+ */
+public class Git2024261 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
