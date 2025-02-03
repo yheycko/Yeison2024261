d4787980406313fe392450149660c1b36ceb1382
yheyc <yheyc@Yheycko>
2025-02-03 17:47:07 +0000

    Including previous Strings work

--- src/main/java/com/mycompany/git2024261/Git2024261.java
+++ src/main/java/com/mycompany/git2024261/Git2024261.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.git2024261;
 
+import java.util.Scanner;
+
 /**
  *
  * @author yheyc
@@ -13,7 +15,32 @@ public class Git2024261 {
     public static void main(String[] args) {
        
         System.out.println("YeisonQuinteros2024261");
-        
+       
         System.out.println("Hello World!");
+        
+        // Previous strings work
+
+        double ans, num2;
+        int num1;
+        
+        Scanner myKB = new Scanner(System.in);
+        
+        System.out.println("Please enter 1st number");
+        num1 = myKB.nextInt();
+        
+        System.out.println("Please enter 2nd number");
+        num2 = myKB.nextInt();
+        
+        //check to see if second num is a zero
+        if (num2 == 0){
+            
+            System.out.println("CANNOT DIVIDE BY ZERO!");
+        }
+        else {
+            //all is OK
+            ans = num1 / num2;
+            System.out.println("The result of " + num1 + " divided by " + num2 + " is " + ans);
+            
+        }
     }
 }
